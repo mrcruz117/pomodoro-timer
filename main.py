b63f4ec0cdd4986366c55b4d176039257f892aa1
mrcruz117 <mrcruz1989@gmail.com>
2022-04-19 14:55:34 -0400

    made countdown function

--- main.py
+++ main.py
@@ -16,10 +16,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def countdown(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40), bg=YELLOW)
 title_label.grid(column=1, row=0)
@@ -27,9 +34,9 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=FONT_NAME)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=FONT_NAME)
 canvas.grid(column=1, row=1)
-
+countdown(5)
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0)
