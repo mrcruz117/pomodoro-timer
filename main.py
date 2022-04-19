a560ec8c4177f4d297ddb63a01233ea9580b5144
mrcruz117 <mrcruz1989@gmail.com>
2022-04-19 14:13:37 -0400

    Style changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAME = "Roboto"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -19,11 +19,11 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
-canvas = Canvas(width=200, height=224)
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(103, 130, text="00:00", fill="white", font="roboto")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=FONT_NAME)
 canvas.pack()
 
 window.mainloop()
