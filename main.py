26b411e74c25f4e306a32bba3c57526f5d8f19e2
mrcruz117 <mrcruz1989@gmail.com>
2022-04-19 15:47:28 -0400

    timer button is formatting correctly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -10,14 +11,20 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 def countdown(count):
-    canvas.itemconfig(timer_text, text=count)
+    count_minute = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_sec}")
     if count > 0:
         window.after(1000, countdown, count - 1)
 
@@ -27,7 +34,6 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
 title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40), bg=YELLOW)
 title_label.grid(column=1, row=0)
 
@@ -36,8 +42,8 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=FONT_NAME)
 canvas.grid(column=1, row=1)
-countdown(5)
-start_button = Button(text="Start", highlightthickness=0)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
